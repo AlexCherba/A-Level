32312fda961ef94548b99ee2d69d21e7863f238d
AlexCherba <alexander.cherba@gmail.com>
2017-11-27 21:58:43 +0200

    create new program work with csv

--- src/classroom/csv/ExcelTable.java
+++ src/classroom/csv/ExcelTable.java
@@ -1,4 +1,21 @@
 package classroom.csv;
 
-public class ExcelTable {
+import java.util.ArrayList;
+
+public abstract class ExcelTable {
+    private static ArrayList<ArrayList> arrayTable;
+
+    public static void addUser(String firstName, String lastName, String age){
+        ArrayList<String> newUser = new ArrayList<>();
+        newUser.add(firstName);
+        newUser.add(lastName);
+        newUser.add(age);
+        arrayTable.add(newUser);
+    }
+    public static void initial(String inFile){
+         arrayTable = FileUtils.read(inFile);
+    }
+    public static ArrayList<ArrayList> get(){
+        return arrayTable;
+    }
 }
--- src/classroom/csv/Main.java
+++ src/classroom/csv/Main.java
@@ -1,11 +1,53 @@
 package classroom.csv;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         String inFile = "D:\\Workspace\\Java\\IDEA\\a-level\\src\\classroom\\csv\\input.csv";
         String outFile = "D:\\Workspace\\Java\\IDEA\\a-level\\src\\classroom\\csv\\output.csv";
+        String firstName;
+        String lastName;
+        String age;
 
-        //FileUtils.read(inFile);
-        FileUtils.print(FileUtils.read(inFile));
+        ExcelTable.initial(inFile);
+        //FileUtils.print(FileUtils.read(inFile));
+        //FileUtils.print(ExcelTable.get());
+
+        Scanner scanner01 = new Scanner(System.in);
+
+        while (true) {
+            System.out.println(
+                    "Что вы планируете делать?\n" +
+                            "1. Добавить нового пользователя\n" +
+                            "2. Изменить существующего пользователя\n" +
+                            "3. Выход");
+
+            //  View menu from select file or generate random Zoo
+
+            switch (scanner01.nextInt()) {
+                case 1:
+                    System.out.print("Имя: ");
+                    firstName = scanner01.next();
+                    System.out.println();
+                    System.out.print("Фамилия: ");
+                    lastName = scanner01.next();
+                    System.out.println();
+                    System.out.print("Возраст: ");
+                    age = scanner01.next();
+                    System.out.println();
+                    ExcelTable.addUser(firstName,lastName,age);
+                    FileUtils.print(ExcelTable.get());
+                    continue;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    System.out.println("Не катит, 1 или 2 плиз!");
+                    continue;
+            }
+            break;
+        }
     }
 }
