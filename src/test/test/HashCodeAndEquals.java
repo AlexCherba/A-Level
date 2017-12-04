cc77ad084115a98bff60a258e0563cc258f479ea
AlexCherba <alexander.cherba@gmail.com>
2017-12-04 21:03:16 +0200

    work work work

--- src/test/test/HashCodeAndEquals.java
+++ src/test/test/HashCodeAndEquals.java
@@ -4,9 +4,17 @@ public class HashCodeAndEquals {
     public static void main(String[] args) {
         BlackBox object1 = new BlackBox(5, 10);
         BlackBox object2 = new BlackBox(5, 10);
+        System.out.println("HashCode object1:" + object1.hashCode());
+        System.out.println("HashCode object2:" + object2.hashCode());
+        System.out.println("link object1: " + object1);
+        System.out.println("link object2: " + object2);
+        object1.varA ++;
+        System.out.println("HashCode object1:" + object1.hashCode());
+        System.out.println("HashCode object2:" + object2.hashCode());
+        System.out.println("link object1: " + object1);
+        System.out.println("link object2: " + object2);
+
         System.out.println("Equals? " + object1.equals(object2));
-        System.out.println("HashCode object1" + object1.hashCode());
-        System.out.println("HashCode object2" + object2.hashCode());
         System.out.println("Hashcode = ? " + (object1.hashCode() == object2.hashCode()));
     }
 }
@@ -18,5 +26,23 @@ class BlackBox {
     BlackBox(int varA, int varB) {
         this.varA = varA;
         this.varB = varB;
+    }
+
+    //@Override
+    public boolean equals1(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BlackBox blackBox = (BlackBox) o;
+
+        if (varA != blackBox.varA) return false;
+        return varB == blackBox.varB;
+    }
+
+    //@Override
+    public int hashCode1() {
+        int result = varA;
+        result = 31 * result + varB;
+        return result;
     }
 }
